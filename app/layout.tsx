2b4775cd834784e2964dad10899e1b1646933f47
vandad1901 <vandad1901@gmail.com>
2024-06-07 15:41:56 +0330

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import "./globals.css";
+
+import { Inter } from "next/font/google";
+import type { Metadata } from "next";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+	title: "Primal To Dual Converter",
+	description: "An applet made to convert an LP from it's primal form to it's dual form",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body className={inter.className}>{children}</body>
+		</html>
+	);
+}
